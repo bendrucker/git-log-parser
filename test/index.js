56f285be81226a772934016ce8d7f8db71576706
Ben Drucker <bvdrucker@gmail.com>
2014-11-22 22:40:24 -0500

    test(parser): add basic tests for streaming and typecasting functionality

---
+++ test/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var expect    = require('chai').expect;
+var logParser = require('../');
+
+describe('git-log-parser', function () {
+
+  var commits = [];
+  before(function (done) {
+    logParser.parse(['--', './test/versioned/*'])
+      .on('data', function (data) {
+        commits.push(data);
+      })
+      .on('error', done)
+      .on('finish', done);
+  });
+
+  it('creates a stream of commit objects', function () {
+    expect(commits).to.have.length(2);
+  });
+
+  it('types dates', function () {
+    expect(commits[0].author.date).to.be.an.instanceOf(Date);
+    expect(commits[0].committer.date).to.be.an.instanceOf(Date);
+  });
+
+});
